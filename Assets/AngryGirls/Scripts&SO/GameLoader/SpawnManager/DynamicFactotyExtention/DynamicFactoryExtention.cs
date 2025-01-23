e694b1206de9eac15a7e6adb79bd56bcce042dc5
DigitalCatharsis <shinegumy@gmail.com>
2025-01-23 19:25:39 +0300

    audio fix

--- Assets/AngryGirls/Scripts&SO/GameLoader/SpawnManager/DynamicFactotyExtention/DynamicFactoryExtention.cs
+++ Assets/AngryGirls/Scripts&SO/GameLoader/SpawnManager/DynamicFactotyExtention/DynamicFactoryExtention.cs
@@ -182,7 +182,7 @@ using UnityEngine;
             int switchCaseIndex = poolObjectLoaderCode.IndexOf("objType switch");
             if (switchCaseIndex == -1)
             {
-                Debug.LogError("Cant find objType switch in  PoolObjectLoader ");
+                Debug.LogError("objType switch");
                 return;
             }
 
@@ -266,40 +266,6 @@ using UnityEngine;
             }
 
             return path;
-        }
-    }
-
-    [System.Serializable]
-    public class SerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
-    {
-        [SerializeField]
-        private List<TKey> keys = new List<TKey>();
-
-        [SerializeField]
-        private List<TValue> values = new List<TValue>();
-
-        // save the dictionary to lists
-        public void OnBeforeSerialize()
-        {
-            keys.Clear();
-            values.Clear();
-            foreach (KeyValuePair<TKey, TValue> pair in this)
-            {
-                keys.Add(pair.Key);
-                values.Add(pair.Value);
-            }
-        }
-
-        // load dictionary from lists
-        public void OnAfterDeserialize()
-        {
-            this.Clear();
-
-            if (keys.Count != values.Count)
-                throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable.", keys.Count, values.Count));
-
-            for (int i = 0; i < keys.Count; i++)
-                this.Add(keys[i], values[i]);
         }
     }
 }
